5f9df8a0c41ee4e952497348d3516fa1a1db4cbb
minku0809-pixel <minku0809@naver.com>
2026-05-30 15:24:52 +0900

    (2026.5.30) 시퀀스 포인트

--- Program/Source.C
+++ Program/Source.C
@@ -1,4 +1,5 @@
 ﻿#include <stdio.h>
+#include <stdarg.h>
 
 void initialize()
 {
@@ -15,7 +16,6 @@ void render(int life)
 	printf("\n");
 }
 
-
 void swap(int * argument_x, int * argument_y)
 {
 	int temporary = *argument_y;
@@ -25,7 +25,34 @@ void swap(int * argument_x, int * argument_y)
 	*argument_x = temporary;
 }
 
-void main()
+int absolute(int x)
+{
+	return (x < 0) ? -x : x;
+}
+
+void execute(int count)
+{
+	if (count <= 0)
+	{
+		return;
+	}
+
+	execute(count -1);
+
+	printf("count : %d\n", count);
+}
+
+double average(int size, ...)
+{
+	// va_list : 가변 인수의 메모리 주소를 저장하는 포인터 변수입니다.
+	va_list pointer;
+
+	// va_start : 가변 인수를 가져올 수 있도록 포인터를 설정하는 함수입니다.
+	va_start(pointer, size);
+s
+}
+
+int main()
 {
 
 #pragma region 함수
@@ -62,5 +89,33 @@ void main()
 	// 매개 변수의 자료형이 서로 일치해야 합니다.
 #pragma endregion
 
+#pragma region 반환형
+	// 함수가 실행을 마치고, 호출한 쪽으로 어떤 자료형의 값을
+	// 반환할 지 미리 알려주는 형식입니다.
+
+	// printf("% d\n", absolute(-7));
+
+	// 함수의 경우 자료형과 반환하는 값의 형태가
+	// 일치하지 않으면 원하는 값을 얻을 수 없습니다.
+#pragma endregion
+
+#pragma region 재귀 함수
+	// 어떤 함수에서 자신을 다시 호출하여 작업을 
+	// 수행하는 함수입니다.
+
+	// execute(3);
+
+	// 재귀 함수는 함수를 계속 호출하기 때문에 스택 영역에
+	// 메모리가 계속 쌓이게 되므로 스택 오버플로우가 발생합니다.
+#pragma endregion
+
+#pragma region 가변 인수 목록
+	// 고정되어 있는 매개 변수 외에 개수가 정해지지 않은
+	// 인수를 추가로 받을 수 있는 인수입니다.
+
+#pragma endregion
+
+
+	return 0;
 
 }
