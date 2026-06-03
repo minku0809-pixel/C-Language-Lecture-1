37f23a672dbbb1058785a5bf5d079ccdc13b6ed2
minku0809-pixel <minku0809@naver.com>
2026-06-03 10:35:49 +0900

    (2026.6.3) 메모리 단편화

---
+++ Class12th (Dynamic Alloction)/Source.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+#pragma region 동적 할당
+	// 프로그램을 실행하는 중에 필요한 만큼 메모리를
+	// 할당하는 작업입니다.
+
+#pragma region memory allocation
+	// 프로그램이 실행되는 동안 한 영역에 메모리를 요청하고
+	// 해당하는 메모리의 시작 주소를 반환하는 함수입니다.
+
+	// int* pointer = (int *)malloc(sizeof(int));
+	// 
+	// *pointer = 100;
+	// 
+	// printf("pointer 변수가 가리키는 값 : %d\n", *pointer);
+	// 
+	// free(pointer);
+	// 
+	// printf("pointer 변수가 가리키는 값 : %d\n", *pointer);
+
+	// 이미 해제된 메모리를 가리키는 포인터의 경우 예기치 못한
+	// 동작을 유발할 수 있습니다.
+#pragma endregion
+
+#pragma region contiguous allocation
+	// 연속된 메모리 공간을 확보하고, 모든 요소의 값을 0으로
+	// 초기화한 뒤 할당된 메모리의 시작 주소를 반환하는 함수입니다.
+
+	// int * address = calloc(sizeof(int), 5);
+	// 
+	// for (int i = 0; i < 5; i++)
+	// {
+	// 	printf("address[%d] : %d\n", i, address[i]);
+	// }
+	// 
+	// free(address);
+
+#pragma endregion
+
+#pragma region resize allocation
+	// 이미 할당된 메모리 공간의 크기를 확장하거나, 변경할 수 있는
+	// 함수입니다.
+
+	// int* reference = (int*)malloc(sizeof(int) * 3);
+	// 
+	// for (int i = 0; i < 3; i++)
+	// {
+	// 	reference[i] = (i + 1) * 10;
+	// 
+	// 	printf("reference[%d] = %d\n", i, reference[i]);
+	// }
+	// 
+	// int * temporary = realloc(reference, sizeof(int) * 5);
+	// 
+	// if (temporary == NULL)
+	// {
+	// 	free(reference);
+	// 
+	// 	return 1;
+	// }
+	// 
+	// reference = temporary;
+	// 
+	// reference[3] = 40;
+	// reference[4] = 50;
+	// 
+	// printf("\n");
+	// 
+	// for (int i = 0; i < 5; i++)
+	// {
+	// 	printf("reference[%d] = %d\n", i, reference[i]);
+	// }
+	// 
+	// free(reference);
+
+#pragma endregion
+
+
+	// 동적 할당은 실행 시간에 가변적으로 메모리의 크기를
+	// 변경시킬 수 있으며, 동적으로 메모리의 크기를 할당할 때
+	// 바이트 단위로 지정합니다.
+#pragma endregion
+
+
+	return 0;
+}
